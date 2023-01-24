cf3666720ea6be11d8f62f75e92cee495d5dfa5b
dejesusf <dejesusf@uw.edu>
2023-01-23 20:39:38 -0800

    completed inquirer prompts and writeFile

--- index.js
+++ index.js
@@ -1,16 +1,72 @@
 // Include packages needed for this application
 const inquirer= require('inquirer');
 const fs= require('fs');
+const generateReadMe= ({title,description,installation,usage,credits,license})=>{
+    return ` # ${title}
 
+    ## Desription
+    ${description}
 
-// TODO: Create an array of questions for user input
-const questions = [];
+    ## Table of Contents
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
 
-// TODO: Create a function to write README file
-function writeToFile(filename, data) {}
+    ## Installation
+    ${installation}
+    
+    ## Usage
+    ${usage}
 
-// TODO: Create a function to initialize app
-function init() {}
+    To add a screenshot, use the relative filepath and add it here using the following syntax:
+    ![alt text](images/screenshot.png)
 
-// Function call to initialize app
-init();
+    ## Credits
+    ${credits}
+
+    ## License
+    ${license}
+    `
+}
+
+// Create an array of questions for user input
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Briefly describe the what, why, and how of your project.'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide any instructions and examples for use. Enter N/A if not applicable.',
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'List any collaborators, if any. Enter N/A if not applicable.',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'If there were collaborators, please list their GitHub profiles. Enter N/A if not applicable.',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please choose a license for your project',
+            choices: ['None','Apache License 2.0','GNU GPLv3', 'GNU AGPLv3', 'GNU LGPLv3', 'MIT', 'ISC License', 'Mozilla Public License 2.0', 'Boost Software License 1.0'],
+        },
+    ])
+    .then((response)=>
+    // Create a function to write README file
+    fs.writeFile('UserREADME.md', generateReadMe(response), (err)=> (err ? console.error(err) : console.log('Generated professional README!'))
+    )
+);
